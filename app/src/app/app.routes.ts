9c7b3637568cb63fa710f1b30c5861537e750e46
Shubham Bansal <shubham.bansal2@globallogic.com>
2022-07-06 11:53:38 +0000

    APPs

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -38,13 +38,9 @@ import { RecentFilesComponent } from './components/recent-files/recent-files.com
 import { SharedFilesComponent } from './components/shared-files/shared-files.component';
 import { DetailsComponent } from './components/details/details.component';
 import { HomeComponent } from './components/home/home.component';
-import { ViewProfileComponent } from './components/view-profile/view-profile.component';
 
 export const APP_ROUTES: Routes = [
-  {
-    path: 'profile',
-    component: ViewProfileComponent
-  },
+
   {
     path: 'blank',
     component: BlankPageComponent
